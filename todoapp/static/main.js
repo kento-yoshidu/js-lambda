6be306585d752d2550b23096131b61fddff8b6b0
kento <kento@toriwatari.bird>
2023-01-28 22:06:19 +0900

    ✨ Update

---
+++ todoapp/static/main.js
@@ -0,0 +1,8 @@
+window.alert("Hrllo JavaScript");
+
+const API_URL_PREFIX = "https:j1x5q4zn51.execute-api.ap-northeast-1.amazonaws.com/tasks"
+
+const taskTitleInputElement = document.getElementById("task-list-input")
+const taskAddButtonElement = document.getElementById("task-add-button")
+
+
