97a500fc7e82b36a914a85b56d5942e187295237
kento <kento@toriwatari.bird>
2023-01-28 19:03:48 +0900

    ✨ Update

--- todoapp/src/taskHandler.js
+++ todoapp/src/taskHandler.js
@@ -1,6 +1,25 @@
 import { DynamoDB } from "aws-sdk";
 import crypt from "crypto"
 
+export async function list(event, context) {
+  const dynamoDB = new DynamoDB({
+    region: "ap-northeast-1"
+  })
+
+  const result = await dynamoDB.scan({
+    TableName: "tasks",
+  }).promise()
+
+  const tasks = result.Items.map((item) => {
+    return {
+      id: item.id.S,
+      title: item.title.S
+    }
+  })
+
+  return { tasks: tasks }
+}
+
 export async function post(event, context) {
   const requestBody = JSON.parse(event.body);
 
